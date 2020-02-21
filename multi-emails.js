6d6e538f80a34ad80f488aea5264b890835c930c
DSerhiy <dserhiyweb@gmail.com>
2020-02-21 22:50:57 +0200

    Add field visual validation

--- multi-emails.js
+++ multi-emails.js
@@ -1,42 +1,51 @@
 class MultiEmailsInput {
 	constructor(inputEl) {
-		this.data = inputEl.value.split(',');
+		this.data = inputEl.value === '' ? []: inputEl.value.split(',');
 		this.__inputEl = inputEl;
-		this.__dataInputEl = document.createElement('input');
+		this.__dataInputEl = document.createElement('input');	
+		this.__errorLable = inputEl.nextElementSibling.classList.contains('error') ? inputEl.nextElementSibling : null;
 		this.__isOneline = this.__inputEl.getAttribute('data-role') === 'multi-emails-input--oneline' ? true : false;
-		this.__init();
+		this.__init();		
 	}
 
-	__init() {
-
-		this.__dataInputEl.setAttribute('type', 'hidden');
-		this.__dataInputEl.value = this.data.join(',');
-		this.__dataInputEl.name = this.__inputEl.name;
-		this.__dataInputEl.id = this.__inputEl.id;
-
-		// wrap input in multi-email-container
-		const wrapperEl = document.createElement('div');
-		wrapperEl.classList.add('sd_multi-emails_container', 'form-control');
-
-		if (this.__isOneline)
-			wrapperEl.classList.add('sd_multi-emails_container--oneline');
-
-		this.__inputEl.removeAttribute('class');
-		this.__inputEl.removeAttribute('value');
-		this.__inputEl.parentElement.insertBefore(wrapperEl, this.__inputEl);
-		wrapperEl.appendChild(this.__inputEl);		
-
-		this.__inputEl.removeAttribute('name');
-		this.__inputEl.removeAttribute('id');
-
-		this.__inputEl.parentElement.insertBefore(this.__dataInputEl, this.__inputEl);
+	__init() {	
+		this.__setUpDataInputEl();
+		this.__setUpInputEl();
+		this.__setUpErrorLableEl();	
+		this.__setUpWrapperEl();
 		
 		this.data.forEach((email) => {
-			this.__createNewEmail(email);
-		});
+			this.__createEmailTag(email);
+		});			
+	}
 
+	__setUpDataInputEl() {
+		this.__dataInputEl.setAttribute('type', 'text');
+		this.__dataInputEl.setAttribute('required', this.__inputEl.required);
+		this.__dataInputEl.style.display = 'none';		
+		this.__dataInputEl.value = this.data.join(',');
+		this.__dataInputEl.name = this.__inputEl.name;
+		this.__dataInputEl.id = this.__inputEl.id;	
+	}
+
+	__setUpErrorLableEl() {		
+		if(this.__errorLable) {
+			this.__errorLable.style.display = 'none';
+			this.__dataInputEl.addEventListener('invalid', (e)=>{
+				e.preventDefault();
+				this.__errorLable.style.display = 'block';			
+			});
+		}		
+	}
+
+	__setUpInputEl() {
 		//reset input value for correct display		 
 		this.__inputEl.value = '';
+		this.__inputEl.removeAttribute('required');
+		this.__inputEl.removeAttribute('class');
+		this.__inputEl.removeAttribute('value');
+		this.__inputEl.removeAttribute('name');
+		this.__inputEl.removeAttribute('id');	
 
 		this.__inputEl.addEventListener('keydown', (e) => {
 			if (this.__inputEl.classList.contains('sd_text--red'))
@@ -47,41 +56,44 @@ class MultiEmailsInput {
 			const emailValue = e.currentTarget.value;
 
 			if (this.__validateEmail(emailValue)) {
-				e.currentTarget.value = '';
-				this.data.push(emailValue);
-				this.__dataInputEl.value = this.data.join(',');
-				this.__createNewEmail(emailValue);
+				this.__addEmail(emailValue);				
 			} else {
 				this.__inputEl.classList.add('sd_text--red');
 			}
 		});
 
-		this.__inputEl.parentElement.addEventListener('click', () => this.__inputEl.focus());
-
-		this.__inputEl.addEventListener('focus', (e) => {
-			e.target.parentElement.classList.toggle('sd_multi-emails_container--onfocus');
-		});
-
 		this.__inputEl.addEventListener('focusout', (e) => {
-			e.target.parentElement.classList.toggle('sd_multi-emails_container--onfocus');
-
-			if (this.__inputEl.classList.contains('sd_text--red'))
-				this.__inputEl.classList.remove('sd_text--red');
+			e.target.parentElement.classList.toggle('sd_multi-emails_container--onfocus');			
 
 			const emailValue = this.__inputEl.value;
 
 			if (this.__validateEmail(emailValue)) {
-				this.__inputEl.value = '';
-				this.data.push(emailValue);
-				this.__dataInputEl.value = this.data.join(',');
-				this.__createNewEmail(emailValue);
+				this.__addEmail(emailValue);
 			} else {
 				this.__inputEl.value = '';
 			}
 		});
+
+		this.__inputEl.addEventListener('focus', (e) => {
+			e.target.parentElement.classList.toggle('sd_multi-emails_container--onfocus');
+		});
 	}
 
-	__createNewEmail(email) {
+	__setUpWrapperEl() {
+		// wrap input in multi-email-container
+		const wrapperEl = document.createElement('div');
+		wrapperEl.classList.add('sd_multi-emails_container', 'form-control');
+
+		if (this.__isOneline) wrapperEl.classList.add('sd_multi-emails_container--oneline');
+		
+		this.__inputEl.parentElement.insertBefore(wrapperEl, this.__inputEl);
+		wrapperEl.appendChild(this.__inputEl);		
+		this.__inputEl.parentElement.insertBefore(this.__dataInputEl, this.__inputEl);		
+
+		wrapperEl.parentElement.addEventListener('click', () => this.__inputEl.focus());
+	}
+
+	__createEmailTag(email) {
 		const newEmailEl = document.createElement('div');
 		newEmailEl.classList.add('sd_email-box');
 
@@ -100,17 +112,21 @@ class MultiEmailsInput {
 			newEmailEl.remove();
 		});
 
+		return newEmailEl;		
+	}
+
+	__addEmail(email) {
+		this.__inputEl.value = '';
+		this.data.push(email);
+		this.__dataInputEl.value = this.data.join(',');
 		// Mount newEmailEl into DOM
-		this.__inputEl.before(newEmailEl);
+		this.__inputEl.before(this.__createEmailTag(email));
+		this.data.length === 1? this.__errorLable.style = 'display:none': true;
 	}
 
 	__validateEmail(email) {
 		const pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		return pattern.test(email);
-	}
-
-	getData() {
-		return this.data;
 	}
 }
 
